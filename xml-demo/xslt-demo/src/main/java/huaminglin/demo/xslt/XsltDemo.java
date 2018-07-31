232dd2088f18b23e0803342e90e3fef92d8d723d
huaminglin <40079294+huaminglin@users.noreply.github.com>
2018-07-31 06:52:13 -0400

    After use StreamSource insteadof DOMSource, Java XSLT works
    javax.xml.transform.stream.StreamSource
    javax.xml.transform.dom.DOMSource

--- xml-demo/xslt-demo/src/main/java/huaminglin/demo/xslt/XsltDemo.java
+++ xml-demo/xslt-demo/src/main/java/huaminglin/demo/xslt/XsltDemo.java
@@ -1,30 +1,26 @@
 package huaminglin.demo.xslt;
 
-import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 
 public class XsltDemo {
     public static String xslt(InputStream style, InputStream model) throws TransformerException, ParserConfigurationException, IOException, SAXException {
-        Document documentStyle = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(style);
-        DOMSource styleSource = new DOMSource(documentStyle);
+        StreamSource styleSource = new StreamSource(style);
         Transformer transformer = TransformerFactory.newInstance().newTransformer(styleSource);
         transformer.setOutputProperty(OutputKeys.METHOD, "html");
         transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 
-        Document documentModel = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(model);
-        DOMSource modelSource = new DOMSource(documentModel);
+        StreamSource modelSource = new StreamSource(model);
         StringWriter sw = new StringWriter();
         StreamResult streamResult = new StreamResult(sw);
 
