7bb881f042129f479d3e7d0d86ce410e978fa223
huaminglin <40079294+huaminglin@users.noreply.github.com>
2018-08-22 09:17:03 -0400

    Add GenericTypeDome to show what is java.lang.reflect.ParameterizedType

---
+++ jdk-demo/src/main/java/huaminglin/demo/jdk/generic/GenericTypeDemo.java
@@ -0,0 +1,49 @@
+package huaminglin.demo.jdk.generic;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.net.UnknownHostException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+public class GenericTypeDemo {
+    public static class MyCallable implements Callable<Map<String, List<String>>> {
+
+        @Override
+        public Map<String, List<String>> call() throws Exception {
+            return null;
+        }
+    }
+    public static void printParameterizedType(ParameterizedType type) {
+        System.out.println("Generic type: " + type);
+        Type[] genericTypes = type.getActualTypeArguments();
+        for (Type genericType : genericTypes) {
+            if (genericType instanceof ParameterizedType) {
+                printParameterizedType((ParameterizedType) genericType);
+            } else {
+                System.out.println("Type: " + genericType);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Type[] types = MyCallable.class.getGenericInterfaces();
+        for (Type type : types) {
+            if (type instanceof ParameterizedType) {
+                printParameterizedType((ParameterizedType) type);
+            } else {
+                System.out.println("Type: " + type);
+            }
+        }
+//        Generic type: java.util.concurrent.Callable<java.util.Map<java.lang.String, java.util.List<java.lang.String>>>
+//        Generic type: java.util.Map<java.lang.String, java.util.List<java.lang.String>>
+//        Type: class java.lang.String
+//        Generic type: java.util.List<java.lang.String>
+//        Type: class java.lang.String
+    }
+}
