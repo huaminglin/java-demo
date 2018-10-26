c1713188209713eadfb77329319b84dca11e91e5
huaminglin <40079294+huaminglin@users.noreply.github.com>
2018-10-26 07:20:32 -0400

    Add wsdl-client-dispatch-demo to show how HTTPBinding.HTTP_BINDING works

---
+++ wsdl-demo/wsdl-client-dispatch-demo/src/main/java/huaminglin/demo/wsdl/client/dispatch/chain/LogHandler.java
@@ -0,0 +1,47 @@
+package huaminglin.demo.wsdl.client.dispatch.chain;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.ws.handler.LogicalHandler;
+import javax.xml.ws.handler.LogicalMessageContext;
+import javax.xml.ws.handler.MessageContext;
+import java.io.StringWriter;
+
+public class LogHandler implements LogicalHandler<LogicalMessageContext> {
+
+    private String getContentAsString(LogicalMessageContext context) {
+        TransformerFactory factory = TransformerFactory.newInstance();
+        Transformer transformer = null;
+        try {
+            transformer = factory.newTransformer();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        }
+        StringWriter sw = new StringWriter();
+        try {
+            transformer.transform(context.getMessage().getPayload(), new StreamResult(sw));
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+        return sw.toString();
+    }
+
+    public void close(MessageContext context) {
+        System.out.println("void close(MessageContext context)");
+    }
+
+    public boolean handleFault(LogicalMessageContext context) {
+        System.out.println("boolean handleFault(LogicalMessageContext context)");
+        System.out.println(getContentAsString(context));
+        return true;
+    }
+
+    public boolean handleMessage(LogicalMessageContext context) {
+        System.out.println("boolean handleMessage(LogicalMessageContext context)");
+        System.out.println(getContentAsString(context));
+        return true;
+    }
+}
--- wsdl-demo/wsdl-server-demo/src/main/java/huaminglin/demo/wsdl/server/RestWSProvider.java
+++ wsdl-demo/wsdl-server-demo/src/main/java/huaminglin/demo/wsdl/server/RestWSProvider.java
@@ -9,6 +9,13 @@ import javax.xml.ws.WebServiceProvider;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.http.HTTPBinding;
 import java.io.StringReader;
+import java.io.StringWriter;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import java.util.Map;
 
 @WebServiceProvider
 @BindingType(value= HTTPBinding.HTTP_BINDING)
@@ -16,11 +23,42 @@ public class RestWSProvider implements Provider<Source> {
     @javax.annotation.Resource(type=Object.class)
     protected WebServiceContext wsContext;
 
+    private String getString(Source source) {
+        TransformerFactory factory = TransformerFactory.newInstance();
+        Transformer transformer = null;
+        try {
+            transformer = factory.newTransformer();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        }
+        StringWriter sw = new StringWriter();
+        try {
+            transformer.transform(source, new StreamResult(sw));
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+        return sw.toString();
+    }
+
+    private String makeMsg(MessageContext messageContext) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Request method: " + messageContext.get(MessageContext.HTTP_REQUEST_METHOD));
+        sb.append(", Path: "+ messageContext.get(MessageContext.PATH_INFO));
+        sb.append(", Query string: " + messageContext.get(MessageContext.QUERY_STRING));
+        sb.append(", User agent: " + ((Map)messageContext.get(MessageContext.HTTP_REQUEST_HEADERS)).get("user-agent"));
+        sb.append(", Content type: " + ((Map)messageContext.get(MessageContext.HTTP_REQUEST_HEADERS)).get("content-type"));
+        sb.append(", Http accept: " + ((Map)messageContext.get(MessageContext.HTTP_REQUEST_HEADERS)).get("accept"));
+        sb.append(", Http host: " + ((Map)messageContext.get(MessageContext.HTTP_REQUEST_HEADERS)).get("host"));
+        sb.append(", Connection: " + ((Map)messageContext.get(MessageContext.HTTP_REQUEST_HEADERS)).get("connection"));
+        System.out.println(messageContext);
+        return sb.toString();
+      }
+
     @Override
     public Source invoke(Source request) {
+        String strRequest = getString(request);
         MessageContext messageContext = wsContext.getMessageContext();
-        String path = (String) messageContext.get(MessageContext.PATH_INFO);
-        String content = "<content>" + path + "</content>";
+        String content = "<response><content>" + makeMsg(messageContext) + "</content><request><![CDATA[" + strRequest + "]]]></request></response>";
         // xml must be valid xml.
         // XML reader error: javax.xml.stream.XMLStreamException: ParseError at [row,col]:[1,1]
         // Message: Content is not allowed in prolog.
