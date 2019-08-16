ace4e0c09f1e991c4be6715054d9295c7adf9e51
huaminglin <40079294+huaminglin@users.noreply.github.com>
2019-08-16 08:54:49 -0400

    Add RedissonDemo

---
+++ redis-demo/src/main/java/huaminglin/demo/redis/redisson/MyEntity.java
@@ -0,0 +1,22 @@
+package huaminglin.demo.redis.redisson;
+
+import java.io.Serializable;
+
+public class MyEntity implements Serializable {
+    private static final long serialVersionUID = 5952812854939081811L;
+
+    private String name;
+
+    public MyEntity() {
+        name = "entity1";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
