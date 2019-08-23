53f2817652e0c5e010d87f00a70b23456a8bee34
huaminglin <40079294+huaminglin@users.noreply.github.com>
2019-08-23 09:28:19 -0400

    Add JUnitDemo

---
+++ junit-demo/src/main/java/huaminglin/demo/junit/JUnitDemo.java
@@ -0,0 +1,13 @@
+package huaminglin.demo.junit;
+
+public class JUnitDemo {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
