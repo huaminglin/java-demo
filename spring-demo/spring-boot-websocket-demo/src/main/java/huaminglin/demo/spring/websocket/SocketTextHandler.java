359d09128109877d9a1f57ca361abc581c2b43a2
huaminglin <40079294+huaminglin@users.noreply.github.com>
2019-09-03 08:29:22 -0400

    Add spring-boot-websocket-demo to learn how websocket works

---
+++ spring-demo/spring-boot-websocket-demo/src/main/java/huaminglin/demo/spring/websocket/SocketTextHandler.java
@@ -0,0 +1,37 @@
+package huaminglin.demo.spring.websocket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+@Component
+public class SocketTextHandler extends TextWebSocketHandler {
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message)
+            throws IOException {
+
+        String courierId = message.getPayload();
+        session.sendMessage(new TextMessage("Getting location: " + courierId));
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                try {
+                    session.sendMessage(new TextMessage("Update location: " + courierId));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 3000);
+    }
+
+}
+
---
+++ spring-demo/spring-boot-websocket-demo/src/main/java/huaminglin/demo/spring/websocket/SpringWebSocketDemo.java
@@ -0,0 +1,16 @@
+package huaminglin.demo.spring.websocket;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSocketDemo {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringWebSocketDemo.class, args);
+        System.out.println("Main Thread exits: " + Thread.currentThread().getId() + ", " + Thread.currentThread().getName());
+    }
+
+}
+
