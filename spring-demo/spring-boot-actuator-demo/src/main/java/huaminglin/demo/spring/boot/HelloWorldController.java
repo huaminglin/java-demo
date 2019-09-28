39e2e5deb0f6671c62db23dc3e6f0f7cffc68ada
huaminglin <40079294+huaminglin@users.noreply.github.com>
2019-09-28 08:57:20 -0400

    Demo /actuator/sessions

--- spring-demo/spring-boot-actuator-demo/src/main/java/huaminglin/demo/spring/boot/HelloWorldController.java
+++ spring-demo/spring-boot-actuator-demo/src/main/java/huaminglin/demo/spring/boot/HelloWorldController.java
@@ -1,5 +1,6 @@
 package huaminglin.demo.spring.boot;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,5 +17,17 @@ public class HelloWorldController {
     @ResponseBody
     public Object main() {
         return "Hello World: " + idProvider.getId();
+    }
+
+    @RequestMapping(value = "/session")
+    @ResponseBody
+    public Object main(HttpServletRequest request) {
+        Integer count = (Integer) request.getSession().getAttribute("count");
+        if (count == null) {
+            count = 0;
+        }
+        count += 1;
+        request.getSession().setAttribute("count", count);
+        return "Hello World: " + count;
     }
 }
