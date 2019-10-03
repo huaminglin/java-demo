a1ce12d57ace631c1a6d0d46c654b89ce303395f
huaminglin <40079294+huaminglin@users.noreply.github.com>
2019-10-03 03:54:42 -0400

    @EnableDiscoveryClient is not needed for service registration

--- spring-cloud-demo/spring-cloud-consul-demo/src/main/java/huaminglin/demo/spring/cloud/consul/SpringCloudConsulDemo.java
+++ spring-cloud-demo/spring-cloud-consul-demo/src/main/java/huaminglin/demo/spring/cloud/consul/SpringCloudConsulDemo.java
@@ -2,10 +2,8 @@ package huaminglin.demo.spring.cloud.consul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class SpringCloudConsulDemo {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudConsulDemo.class, args);
