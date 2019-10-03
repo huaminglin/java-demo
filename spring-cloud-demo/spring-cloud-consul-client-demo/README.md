e0781ae043095a6176b2c1b9c1e51098b37bfebe
huaminglin <40079294+huaminglin@users.noreply.github.com>
2019-10-03 04:53:03 -0400

    Add spring-cloud-consul-client-demo
    Use org.springframework.cloud.client.discovery.DiscoveryClient to fetch data from Consul service.

---
+++ spring-cloud-demo/spring-cloud-consul-client-demo/README.md
@@ -0,0 +1,11 @@
+mvn spring-boot:run
+
+#####################################################
+## Output:
+Services:
+consul
+spring-cloud-consul-demo
+spring-cloud-consul-demo-management
+web
+spring-cloud-consul-demo:
+DefaultServiceInstance{instanceId='spring-cloud-consul-demo-8080', serviceId='spring-cloud-consul-demo', host='x.x.x.x', port=8080, secure=false, metadata={secure=false}}
---
+++ spring-cloud-demo/spring-cloud-consul-client-demo/src/main/java/huaminglin/demo/spring/cloud/consul/client/SpringCloudConsulClientDemo.java
@@ -0,0 +1,27 @@
+package huaminglin.demo.spring.cloud.consul.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudConsulClientDemo implements CommandLineRunner {
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConsulClientDemo.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Services:");
+        discoveryClient.getServices().forEach(System.out::println);
+        System.out.println("spring-cloud-consul-demo:");
+        discoveryClient.getInstances("spring-cloud-consul-demo").forEach(System.out::println);
+    }
+}
