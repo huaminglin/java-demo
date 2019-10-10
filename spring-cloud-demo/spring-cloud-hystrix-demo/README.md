c82526374e78fda9dbeccc05469be3a8e8bb0879
huaminglin <40079294+huaminglin@users.noreply.github.com>
2019-10-10 09:04:01 -0400

    Enable Hystrix Dashboard

--- spring-cloud-demo/spring-cloud-hystrix-demo/README.md
+++ spring-cloud-demo/spring-cloud-hystrix-demo/README.md
@@ -113,3 +113,8 @@ cd /var; python -m SimpleHTTPServer 9091
 		}
 	}
 }
+
+## Hystrix Dashboard
+http://127.0.0.1:8080/hystrix/
+
+http://127.0.0.1:8080/actuator/hystrix.stream
--- spring-cloud-demo/spring-cloud-hystrix-demo/src/main/java/huaminglin/demo/spring/cloud/hystrix/SpringCloudHystrixDemo.java
+++ spring-cloud-demo/spring-cloud-hystrix-demo/src/main/java/huaminglin/demo/spring/cloud/hystrix/SpringCloudHystrixDemo.java
@@ -2,11 +2,13 @@ package huaminglin.demo.spring.cloud.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.web.client.RestTemplate;
 
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 @SpringBootApplication
 public class SpringCloudHystrixDemo {
     @Bean
