e05b7f7990289143a01b5f37264e2cd70906868b
Hyman Lin <besthyman@gmail.com>
2020-06-23 11:16:35 +0800

    Move aspect-demo into aop-demo

--- jdk8-demo/src/main/java/huaminglin/demo/jdk8/stream/CommonForkJoinPoolDemo.java
+++ jdk8-demo/src/main/java/huaminglin/demo/jdk8/stream/CommonForkJoinPoolDemo.java
@@ -6,26 +6,28 @@ import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
 public class CommonForkJoinPoolDemo {
-    public static void main(String[] args) {
-        System.out.println("CPU Core: " + Runtime.getRuntime().availableProcessors());
-        System.out.println("CommonPool Parallelism: " + ForkJoinPool.commonPool().getParallelism());
-        System.out.println("CommonPool Common Parallelism: " + ForkJoinPool.getCommonPoolParallelism());
 
+  public static void main(String[] args) {
+    System.out.println("CPU Core: " + Runtime.getRuntime().availableProcessors());
+    System.out.println("CommonPool Parallelism: " + ForkJoinPool.commonPool().getParallelism());
+    System.out.println("CommonPool Common Parallelism: " + ForkJoinPool.getCommonPoolParallelism());
 
-        long firstNum = 1;
-        long lastNum = 10;
+    long firstNum = 1;
+    long lastNum = 10;
 
-        List<Long> aList = LongStream.rangeClosed(firstNum, lastNum).boxed()
-                .collect(Collectors.toList());
+    List<Long> aList = LongStream.rangeClosed(firstNum, lastNum).boxed()
+        .collect(Collectors.toList());
 
-        long start = System.nanoTime();
-        Long reduce = aList.parallelStream().peek(a -> {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }).reduce(0L, Long::sum);
-        System.out.println("Processed in " + (System.nanoTime() - start) /1000/1000 + " milliseconds, result: " + reduce);
-    }
+    long start = System.nanoTime();
+    Long reduce = aList.parallelStream().peek(a -> {
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }).reduce(0L, Long::sum);
+    System.out.println(
+        "Processed in " + (System.nanoTime() - start) / 1000 / 1000 + " milliseconds, result: "
+            + reduce);
+  }
 }
