402a19a3e4938f290537dd0d6f8a37edd3a88852
huaminglin <40079294+huaminglin@users.noreply.github.com>
2021-07-15 06:56:24 -0400

    Update BitShiftDemo

--- datatype-demo/src/main/java/huaminglin/demo/datatype/bit/BitShiftDemo.java
+++ datatype-demo/src/main/java/huaminglin/demo/datatype/bit/BitShiftDemo.java
@@ -7,6 +7,9 @@ Integer.MIN_VALUE
 >>      -1073741824:    11000000000000000000000000000000
 >>>     1073741824:     1000000000000000000000000000000
 ~       2147483647:     1111111111111111111111111111111
+-       -2147483648:    10000000000000000000000000000000
+n & (-n)        -2147483648:    10000000000000000000000000000000
+n & (n-1)       0:      0
 
 -8
 -8:     11111111111111111111111111111000
@@ -14,6 +17,9 @@ Integer.MIN_VALUE
 >>      -4:     11111111111111111111111111111100
 >>>     2147483644:     1111111111111111111111111111100
 ~       7:      111
+-       8:      1000
+n & (-n)        8:      1000
+n & (n-1)       -16:    11111111111111111111111111110000
 
 -1
 -1:     11111111111111111111111111111111
@@ -21,6 +27,9 @@ Integer.MIN_VALUE
 >>      -1:     11111111111111111111111111111111
 >>>     2147483647:     1111111111111111111111111111111
 ~       0:      0
+-       1:      1
+n & (-n)        1:      1
+n & (n-1)       -2:     11111111111111111111111111111110
 
 8
 8:      1000
@@ -28,6 +37,9 @@ Integer.MIN_VALUE
 >>      4:      100
 >>>     4:      100
 ~       -9:     11111111111111111111111111110111
+-       -8:     11111111111111111111111111111000
+n & (-n)        8:      1000
+n & (n-1)       0:      0
 
 Integer.MAX_VALUE
 2147483647:     1111111111111111111111111111111
@@ -35,6 +47,9 @@ Integer.MAX_VALUE
 >>      1073741823:     111111111111111111111111111111
 >>>     1073741823:     111111111111111111111111111111
 ~       -2147483648:    10000000000000000000000000000000
+-       -2147483647:    10000000000000000000000000000001
+n & (-n)        1:      1
+n & (n-1)       2147483646:     1111111111111111111111111111110
 
 
 Note: "-1 >> 1" return "-1", which is not as expected. -1/2, 0 is expected.
@@ -45,6 +60,12 @@ Note: "~(Integer.MIN_VALUE)" return "Integer.MAX_VALUE".
 
 Note: 2's Complement works like a clock.
 "~" works like flip the index from Clock-wise to Anti-clock-wise: ~8 -> -9. (8 - 0) == -(-9 - (-1))
+
+n & (-n): get the rightmost set bit
+n & (n-1): Unset the rightmost set bit
+
+Note: (-Integer.MIN_VALUE) is Integer.MIN_VALUE itself;
+-Integer.MIN_VALUE=-(-(Integer.MAX_VALUE+1))=Integer.MAX_VALUE+1=Integer.MIN_VALUE
 */
 public class BitShiftDemo {
     public static void main(String[] args) {
@@ -59,6 +80,9 @@ public class BitShiftDemo {
             System.out.println(">>\t" + (value >> 1) + ": \t" + Integer.toBinaryString(value >> 1));
             System.out.println(">>>\t" + (value >>> 1) + ": \t" + Integer.toBinaryString(value >>> 1));
             System.out.println("~\t" + (~value) + ": \t" + Integer.toBinaryString(~value));
+            System.out.println("-\t" + (-value) + ": \t" + Integer.toBinaryString(-value));
+            System.out.println("n & (-n)\t" + (value & (-value)) + ": \t" + Integer.toBinaryString((value & (-value))));
+            System.out.println("n & (n-1)\t" + (value & (value-1)) + ": \t" + Integer.toBinaryString((value & (value-1))));
         }
     }
 }
