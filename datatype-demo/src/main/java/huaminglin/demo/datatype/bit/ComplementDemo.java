591dea4f00db0127ed87ff24b6671f1dfcf4f099
Hyman Lin <besthyman@gmail.com>
2021-07-31 00:34:35 +0800

    Demo unsigned byte conversion
    Unsigned byte for -128: 128/10000000
    Unsigned byte for -1: 255/11111111

--- datatype-demo/src/main/java/huaminglin/demo/datatype/bit/ComplementDemo.java
+++ datatype-demo/src/main/java/huaminglin/demo/datatype/bit/ComplementDemo.java
@@ -6,16 +6,32 @@ For instance, for the three-bit number 010/2, the two's complement is 110/2, bec
 
 Integer.MAX_VALUE:       2147483647:     1111111111111111111111111111111
 Integer.MIN_VALUE:      -2147483648:    10000000000000000000000000000000
--1:      11111111111111111111111111111111
+-1:	 11111111111111111111111111111111
+-2:	 11111111111111111111111111111110
+-3:	 11111111111111111111111111111101
+-4:	 11111111111111111111111111111100
+-5:	 11111111111111111111111111111011
+-6:	 11111111111111111111111111111010
+-7:	 11111111111111111111111111111001
+-8:	 11111111111111111111111111111000
+-128:	 11111111111111111111111110000000
 
 Overflow:
 Integer.MAX_VALUE + 1:   -2147483648; Integer.MAX_VALUE + 1 == Integer.MIN_VALUE
 Integer.MIN_VALUE - 1:  2147483647; Integer.MIN_VALUE - 1 == Integer.MAX_VALUE
+Unsigned byte for -128: 128/10000000
+Unsigned byte for -1: 255/11111111
 
 Note: -2147483648:    10000000000000000000000000000000;
 00000000000000000000000000000000;
 So negative representation is one more than positive representation.
-This is why Integer.MIN_VALUE is not -2147483647;
+Positive representation starts from 0, but negative representation starts from -1.
+This is why Integer.MIN_VALUE is -2147483648, not -2147483647.
+
+By checking (-1) - (-8), we can find out that in the negative representation, the role of '0' and '1' is reversed from positive representation;
+that is, '0' is the weight bit in the negative representation; more '0' bit, more far from -1.
+
+From "Unsigned byte for -128: 128/10000000" we know that the unsigned byte has the same binary representation as normal byte.
 
 2's Complement works like a clock.
 Right half is Clock-wise[0, 2147483647]; Left half is Anti-clock-wise[-1, -2147483648].
@@ -29,10 +45,21 @@ public class ComplementDemo {
         System.out.println("Integer.MAX_VALUE:\t " + Integer.MAX_VALUE + ":\t " + Integer.toBinaryString(Integer.MAX_VALUE));
         System.out.println("Integer.MIN_VALUE:\t" + Integer.MIN_VALUE + ":\t" + Integer.toBinaryString(Integer.MIN_VALUE));
         System.out.println("-1:\t " + Integer.toBinaryString(-1));
+        System.out.println("-2:\t " + Integer.toBinaryString(-2));
+        System.out.println("-3:\t " + Integer.toBinaryString(-3));
+        System.out.println("-4:\t " + Integer.toBinaryString(-4));
+        System.out.println("-5:\t " + Integer.toBinaryString(-5));
+        System.out.println("-6:\t " + Integer.toBinaryString(-6));
+        System.out.println("-7:\t " + Integer.toBinaryString(-7));
+        System.out.println("-8:\t " + Integer.toBinaryString(-8));
+        System.out.println("-128:\t " + Integer.toBinaryString(-128));
 
         System.out.println("\nOverflow:");
         System.out.println("Integer.MAX_VALUE + 1:\t " + (Integer.MAX_VALUE + 1) + "; Integer.MAX_VALUE + 1 == Integer.MIN_VALUE");
         System.out.println("Integer.MIN_VALUE - 1:\t" + (Integer.MIN_VALUE - 1) + "; Integer.MIN_VALUE - 1 == Integer.MAX_VALUE");
+
+        System.out.println("Unsigned byte for " + Byte.MIN_VALUE  + ": " + Byte.toUnsignedInt(Byte.MIN_VALUE) + '/' + Integer.toBinaryString(Byte.toUnsignedInt(Byte.MIN_VALUE)));
+        System.out.println("Unsigned byte for " + (-1) + ": " + Byte.toUnsignedInt((byte)-1) + '/' + Integer.toBinaryString(Byte.toUnsignedInt((byte)-1)));
     }
 }
 
